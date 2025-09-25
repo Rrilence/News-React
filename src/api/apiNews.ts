0499805f6e13fec987aa34e68fe62624d7a10deb
Margaret Abakumova <rilence@yandex.ru>
2025-09-25 12:57:33 +0300

    Categories added

--- src/api/apiNews.ts
+++ src/api/apiNews.ts
@@ -3,18 +3,38 @@ import axios from "axios"
 const BASE_URL=process.env.REACT_APP_NEWS_API_URL
 const API_KEY=process.env.REACT_APP_NEWS_API_KEY
 
-export const getNews = async (page_number = 1, page_size = 10) => {
+interface Parametrs {
+    page_number: number,
+    page_size: number,
+    category: string,
+}
+
+export const getNews = async ({page_number = 1, page_size = 10, category}: Parametrs) => {
     try {
         const res = await axios.get(`${BASE_URL}search`, {
             params: {
                 apiKey: API_KEY,
                 page_number,
                 page_size,
+                category,
             }
         })
         return res.data
     } catch(err) {
         console.log(err);
         
+    }
+}
+
+export const getCategories = async () => {
+    try {
+        const res = await axios.get(`${BASE_URL}available/categories`, {
+            params: {
+                apiKey: API_KEY,
+            }
+        })
+        return res.data
+    } catch(err) {
+        console.log(err);
     }
 }
